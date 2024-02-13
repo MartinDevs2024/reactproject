a410b54b6ccfa7c53e97ee64234ee13010a69639
Martin Fernandez <Electronicsmf@aol.com>
2024-02-13 06:57:34 -0500

    Add project files.

---
+++ src/components/Todo.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faPenToSquare } from '@fortawesome/free-regular-svg-icons'
+import { faTrash } from '@fortawesome/free-solid-svg-icons'
+
+
+const Todo = ({task, toggleComplete, deleteTodo, editTodo}) => {
+  return (
+    <div className='Todo'>
+         <p className={`${task.completed ? 
+          'completed': "incompleted"}`}
+          onClick={() => toggleComplete(task.id)}
+          >{task.task}</p>
+         <div>
+              <FontAwesomeIcon className='edit-icon' 
+              icon={faPenToSquare} 
+              onClick={() => editTodo(task.id)}
+              />
+              <FontAwesomeIcon className='delete-icon' 
+              icon={faTrash} onClick={() => 
+                deleteTodo(task.id)}/>
+         </div>
+    </div>
+  )
+}
+
+export default Todo
